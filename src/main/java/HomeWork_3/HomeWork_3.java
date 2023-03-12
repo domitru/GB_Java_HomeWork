e701f3285f31f165e2776b6fc7cc9b3b4aa2938e
Dmitry Basov <dom.it.ru@yandex.ru>
2023-03-12 14:22:13 +0300

    добавил исправил

--- src/main/java/HomeWork_3/HomeWork_3.java
+++ src/main/java/HomeWork_3/HomeWork_3.java
@@ -7,12 +7,13 @@ import java.util.Random;
 
 
 public class HomeWork_3 {
-    //    1. Дан список ArrayList<Integer> целых чисел. Удалить из него нечетные числа.
+
     public static void main(String[] args) {
+        //    1. Дан список ArrayList<Integer> целых чисел. Удалить из него нечетные числа.
         //System.out.println(randomaiser());
         //sortedArrayList(randomaiser());  // 1-е задание, передает рандомно заполненый список.
 
-        ArrayList<String> listDifferent = new ArrayList<>(List.of("string", "s", "5", "10", "-12", "my_value"));
+        ArrayList<String> listDifferent = new ArrayList<>(List.of("!", "Hello", "4", "string", "s", "5", "10", "-12", "my_value"));
         delNumList(listDifferent);
     }
 //    private static ArrayList<Integer> randomaiser(){
@@ -38,33 +39,31 @@ public class HomeWork_3 {
 //        System.out.println(list_2);
 //
 //    }
+
     private static void delNumList(ArrayList<String> list) {
-        System.out.println(list);
+//  2. Дан список ArrayList<String>. Удалить из него все строки,которые являются числами
+//  Пример: ArrayList<String> list = new ArrayList<>(List.of("string", "s", "5", "10", "-12", "my_value"));
+//  -->> ["string", "s", "my_value"]
+        System.out.println(list);  // Печать полученного массива строк
         int i = 0;
         boolean flag = false ;
-        while (i < list.size()){
-            String string = list.get(i);
-            //System.out.print(string + "  ");
-            //System.out.print(string.getClass() + "  ");
-            flag = string.matches("-?\\d+(\\.\\d+)?");
+        while (i < list.size()){  // цикл по длине массива
+            String string = list.get(i);  // Выдергиваем по индексу i из массива строку, кладем в string
+            // System.out.print(string + "  ");
+            // System.out.print(string.getClass() + "  ");
+            flag = string.matches("-?\\d+(\\.\\d+)?"); //проверка строки, число или нет,возвращает
+                                                              // значение false или true.
             //System.out.println(flag);
-            if (flag == true) {
-                System.out.print(string + "  ");
-                list.remove(string);
-                i--;
-                System.out.println(list);
+            if (flag == true) {      // если строка цисловая , то.....
+                // System.out.print(string + "  ");
+                list.remove(string);   //  удаляем из массива строк(list) значение в (string)
+                i--;            //  при удалении идет смещение индексов, -- дает возврат по сммещеию
+                //System.out.println(list);
             }else {
                 //System.out.println(list);
             }
             i++;
-
         }
-
-
+        System.out.println(list);
     }
-//    2. Дан список ArrayList<String>. Удалить из него все строки,
-//    которые являются числами
-//    Пример: ArrayList<String> list = new ArrayList<>(List.of("string", "s", "5", "10", "-12", "my_value"));
-//    ["string", "s", "my_value"]
-
 }
