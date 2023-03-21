51742b44b2cc2834b6c9625f9c595f84f19ff111
Dmitry Basov <dom.it.ru@yandex.ru>
2023-03-21 08:55:18 +0300

    В 3-ем ДЗ почистил код, завел нью класс хавэ 5ть

--- src/main/java/HomeWork_5/HomeWork_5.java
+++ src/main/java/HomeWork_5/HomeWork_5.java
@@ -1,8 +1,22 @@
 package HomeWork_5;
 
 public class HomeWork_5 {
+    /**
+     * Есть тест. Нужно извлечь из него все слова и отсортировать по длине.
+     * Мороз и солнце день чудесный Еще ты дремлешь друг прелестный Пора красавица проснись
+     *
+     * и
+     * ты
+     * Еще
+     * день друг Пора
+     * ..
+     *
+     */
+
     public static void main(String[] args){
 
-
+    }
+    static void collectStats(String text) {
+        // Map<Integer, List<String>> stats = new HashMap<>();
     }
 }
