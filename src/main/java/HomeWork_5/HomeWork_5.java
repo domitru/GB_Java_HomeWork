0a1ea465e3dc0abe13e82fac98a903227c4e24e5
Dmitry Basov <dom.it.ru@yandex.ru>
2023-03-21 13:59:08 +0300

    Задание по 5-му семинару готово

--- src/main/java/HomeWork_5/HomeWork_5.java
+++ src/main/java/HomeWork_5/HomeWork_5.java
@@ -1,5 +1,5 @@
 package HomeWork_5;
-
+import java.util.*;
 public class HomeWork_5 {
     /**
      * Есть тест. Нужно извлечь из него все слова и отсортировать по длине.
@@ -12,11 +12,23 @@ public class HomeWork_5 {
      * ..
      *
      */
-
     public static void main(String[] args){
-
+        String text = "Мороз и солнце день чудесный Еще ты дремлешь друг прелестный Пора красавица проснись";
+        sortByLenght(text);
     }
-    static void collectStats(String text) {
-        // Map<Integer, List<String>> stats = new HashMap<>();
+    private static void sortByLenght(String text){
+        String[] splitText = text.split(" ");
+        Map<Integer, List<String>> stats = new HashMap<>();
+        for (String string : splitText) {
+            List<String> sortString = new ArrayList<String>();
+            if(stats.containsKey(string.length())){
+                sortString = stats.get(string.length());
+            }
+            sortString.add(string);
+            stats.putIfAbsent(string.length(), sortString);
+        }
+        for (Collection<String> s : stats.values()) {
+            System.out.println(s);
+        }
     }
 }
